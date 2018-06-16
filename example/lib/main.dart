f9c034e5b5a4bdd89f82d82999ac45b3b8138684
Romain Rastel <rromain35@gmail.com>
2018-06-16 10:08:35 +0200

    create example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -19,122 +19,131 @@ class MyApp extends StatelessWidget {
 class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    List<Widget> slivers = new List<Widget>();
+
+    slivers.addAll(_buildLists(0, 3));
+    slivers.addAll(_buildGrids(3, 3));
+    slivers.addAll(_buildSideHeaderGrids(6, 3));
+
     return new SimpleScaffold(
-      title: 'Test',
-      child: new CustomScrollView(
-        slivers: List.generate(50, (index) => buildItem(context, index)),
+      title: 'Flutter Sticky Header example',
+      child: new CustomScrollView(slivers: slivers),
+    );
+  }
+
+  List<Widget> _buildLists(int firstIndex, int count) {
+    return List.generate(count, (sliverIndex) {
+      sliverIndex += firstIndex;
+      return new SliverStickyHeader(
+        header: _buildHeader(sliverIndex),
+        sliver: new SliverList(
+          delegate: new SliverChildBuilderDelegate(
+            (context, i) => new ListTile(
+                  leading: new CircleAvatar(
+                    child: new Text('$sliverIndex'),
+                  ),
+                  title: new Text('List tile #$i'),
+                ),
+            childCount: 4,
+          ),
+        ),
+      );
+    });
+  }
+
+  List<Widget> _buildGrids(int firstIndex, int count) {
+    return List.generate(count, (sliverIndex) {
+      sliverIndex += firstIndex;
+      return new SliverStickyHeader(
+        header: _buildHeader(sliverIndex),
+        sliver: new SliverGrid(
+          gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3, crossAxisSpacing: 4.0, mainAxisSpacing: 4.0),
+          delegate: new SliverChildBuilderDelegate(
+            (context, i) => new GridTile(
+                  child: Card(
+                    child: new Container(
+                      color: Colors.green,
+                    ),
+                  ),
+                  footer: new Container(
+                    color: Colors.white.withOpacity(0.5),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: new Text(
+                        'Grid tile #$i',
+                        style: const TextStyle(color: Colors.black),
+                      ),
+                    ),
+                  ),
+                ),
+            childCount: 9,
+          ),
+        ),
+      );
+    });
+  }
+
+  List<Widget> _buildSideHeaderGrids(int firstIndex, int count) {
+    return List.generate(count, (sliverIndex) {
+      sliverIndex += firstIndex;
+      return new SliverStickyHeader(
+        overlapsContent: true,
+        header: _buildSideHeader(sliverIndex),
+        sliver: new SliverPadding(
+          padding: new EdgeInsets.only(left: 60.0),
+          sliver: new SliverGrid(
+            gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3, crossAxisSpacing: 4.0, mainAxisSpacing: 4.0,childAspectRatio: 1.0),
+            delegate: new SliverChildBuilderDelegate(
+              (context, i) => new GridTile(
+                    child: Card(
+                      child: new Container(
+                        color: Colors.orange,
+                      ),
+                    ),
+                    footer: new Container(
+                      color: Colors.white.withOpacity(0.5),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: new Text(
+                          'Grid tile #$i',
+                          style: const TextStyle(color: Colors.black),
+                        ),
+                      ),
+                    ),
+                  ),
+              childCount: 12,
+            ),
+          ),
+        ),
+      );
+    });
+  }
+
+  Widget _buildHeader(int index, {String text}) {
+    return new Container(
+      height: 60.0,
+      color: Colors.lightBlue,
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      alignment: Alignment.centerLeft,
+      child: new Text(
+        text ?? 'Header #$index',
+        style: const TextStyle(color: Colors.white),
       ),
     );
   }
 
-  Widget buildItem(BuildContext context, int index) {
-    if (index == 3) {
-      return new SliverStickyHeader(
-        header: new Container(
-          height: 60.0,
-          color: index.isEven ? Colors.orange : Colors.green,
-          padding: new EdgeInsets.symmetric(horizontal: 16.0),
-          alignment: Alignment.centerLeft,
-          child: new Text(
-            'Header #$index',
-            style: const TextStyle(color: Colors.white),
-          ),
-        ),
-      );
-    } else if (index == 6) {
-      return new SliverStickyHeader(
-        sliver: new SliverList(
-          delegate: new SliverChildBuilderDelegate(
-            (c, i) => new Container(
-                  height: 60.0,
-                  color: Colors.lightBlue,
-                  padding: new EdgeInsets.symmetric(horizontal: 16.0),
-                  alignment: Alignment.centerLeft,
-                  child: new Text(
-                    'Tile #$i',
-                    style: const TextStyle(color: Colors.white),
-                  ),
-                ),
-            childCount: 5,
-          ),
-        ),
-      );
-    } else if (index == 12) {
-      return new SliverStickyHeader(
-        overlapsContent: index == 8,
-        header: new Container(
-          height: 60.0,
-          color: index == 8 ? Colors.deepOrange.withOpacity(0.6) : (index.isEven ? Colors.orange : Colors.green),
-          padding: new EdgeInsets.symmetric(horizontal: 16.0),
-          alignment: Alignment.centerLeft,
-          child: Row(
-            children: <Widget>[
-              new Text('Header #$index', style: const TextStyle(color: Colors.white)),
-              new Expanded(
-                  child: Padding(
-                padding: const EdgeInsets.only(top: 8.0),
-                child: new FlatButton(
-                    onPressed: () => showDialog(
-                          context: context,
-                          builder: (context) => AlertDialog(
-                                title: Text('hello $index'),
-                              ),
-                        ),
-                    child: Text('click')),
-              )),
-            ],
-          ),
-        ),
-        sliver: new SliverList(
-          delegate: new SliverChildBuilderDelegate(
-            (c, i) => Padding(
-                  padding: const EdgeInsets.only(top: 8.0),
-                  child: new FlatButton(
-                    child: new Text('index $i'),
-                    onPressed: () => showDialog(
-                          context: context,
-                          builder: (context) => AlertDialog(
-                                title: Text('Hello $i'),
-                              ),
-                        ),
-                  ),
-                ),
-            childCount: 8,
-          ),
-        ),
-      );
-    } else {
-      return buildHeaderAndContent(context, index);
-    }
-  }
-
-  Widget buildHeaderAndContent(BuildContext context, int index) {
-    return new SliverStickyHeader(
-      overlapsContent: index == 8,
-      header: new Container(
-        height: 60.0,
-        color: index == 8 ? Colors.deepOrange.withOpacity(0.6) : (index.isEven ? Colors.orange : Colors.green),
-        padding: new EdgeInsets.symmetric(horizontal: 16.0),
-        alignment: Alignment.centerLeft,
-        child: new Text(
-          'Header #$index',
-          style: const TextStyle(color: Colors.white),
-        ),
-      ),
-      sliver: new SliverList(
-        delegate: new SliverChildBuilderDelegate(
-          (c, i) => new Container(
-                height: 80.0,
-                color: Colors.lightBlue.withOpacity(0.5),
-                padding: new EdgeInsets.symmetric(horizontal: 16.0),
-                alignment: Alignment.centerLeft,
-                child: new Text(
-                  'Tile #$i',
-                  style: const TextStyle(color: Colors.white),
-                ),
-              ),
-          childCount: 8,
-        ),
+  Widget _buildSideHeader(int index, {String text}) {
+    return new Container(
+      height: 60.0,
+      color: Colors.transparent,
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      alignment: Alignment.centerLeft,
+      child: new CircleAvatar(
+        backgroundColor: Colors.orangeAccent,
+        foregroundColor: Colors.white,
+        child: new Text('$index'),
       ),
     );
   }
